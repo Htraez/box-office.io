cbb0edaebf33506c8bd788c15a865defb5b96d7e
mark <pawinchio@yahoo.com>
2019-05-23 15:11:41 +0700

    update show schedule by pawin

--- public/scripts/movie.js
+++ public/scripts/movie.js
@@ -7,7 +7,7 @@ function BranchOption(){
     var payload = { table:"branch" };
     $.post('/fetchData',payload,(data)=>{
         data.forEach((value,key)=>{
-            // if(value.Position=="manager")
+            
             $("#Branch").append('<option class="form-control-plaintext" value="'+value.BranchNo+'">'+value.BranchName+'</option>');
         });
         console.log(data)
@@ -16,45 +16,28 @@ function BranchOption(){
 
 
 
-function showTheater(cl,data) {
-    var payload = { table:"theatre" };
-    console.log(branch)
+function ScheduleInfo(data) {
+    var payload = { table:"schedule" };
     $.post('/fetchData',payload,(data)=>{
         data.forEach((value,key)=>{
-            if(cl==value.BranchNo)
-            $("#theater").append('<tr class="default-mouse clickTable"><th class="text-white theaterTable" scope="col">'+value.TheatreCode+'</th></tr>');
             
+            $("#Schedule").append('<tr class="default-mouse clickTable"><th class="text-white movieTable" scope="col"value="'+value.ScheduleNo+'">'+value.ScheduleNo+'&emsp;'+value.MovieNo+'&emsp;'+value.TheatreCode+'</th></tr>');
         });
         console.log(data)
     });
     
 }
 
-function datetime(){
-    $('#datetime24').combodate();  
-}
-
-
-function theater(){
-    console.log(this.innerHTML);
-    theater = this.innerHTML;
-    console.log(theater)
-
-}
-
-showmovie();
-
-$(document).on('click',".movieTable",movie);
-$(document).on('click',".branchTable",branch);
-$(document).on('click',".theaterTable",theater);
-$(document).on('click',".clickTable",function(){
-    $(this).addClass('bg-secondary').siblings().removeClass('bg-secondary');
-})
 
 
 
 function callMovieForm(){
     $('#movieAndSchduleForm').show();
+    $('.content-view').hide();
+}
+
+function ShowMovieForm(){
+    $('#ShowMovieAll').show();
     $('.content-view').hide();
 }
 
@@ -70,7 +53,9 @@ function callBackMovieForm(){
 }
 
 $(document).on("click","#createMovie", callMovieForm);
+$(document).on("click","#ShowMovie", ShowMovieForm);
 
 $(document).on("click","#next", callScheduleForm);
 $(document).on("click","#back", callBackMovieForm);
 BranchOption();
+ScheduleInfo();
