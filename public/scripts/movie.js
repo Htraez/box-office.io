55a8fc677df48f920de0c42beb52eebb3cc5e8f4
mark <pawinchio@yahoo.com>
2019-05-23 14:12:19 +0700

    update move.js by pawin

--- public/scripts/movie.js
+++ public/scripts/movie.js
@@ -1,3 +1,8 @@
+var theater
+
+
+
+
 function BranchOption(){
     var payload = { table:"branch" };
     $.post('/fetchData',payload,(data)=>{
@@ -8,6 +13,45 @@ function BranchOption(){
         console.log(data)
     });
 }
+
+
+
+function showTheater(cl,data) {
+    var payload = { table:"theatre" };
+    console.log(branch)
+    $.post('/fetchData',payload,(data)=>{
+        data.forEach((value,key)=>{
+            if(cl==value.BranchNo)
+            $("#theater").append('<tr class="default-mouse clickTable"><th class="text-white theaterTable" scope="col">'+value.TheatreCode+'</th></tr>');
+            
+        });
+        console.log(data)
+    });
+    
+}
+
+function datetime(){
+    $('#datetime24').combodate();  
+}
+
+
+function theater(){
+    console.log(this.innerHTML);
+    theater = this.innerHTML;
+    console.log(theater)
+
+}
+
+showmovie();
+
+$(document).on('click',".movieTable",movie);
+$(document).on('click',".branchTable",branch);
+$(document).on('click',".theaterTable",theater);
+$(document).on('click',".clickTable",function(){
+    $(this).addClass('bg-secondary').siblings().removeClass('bg-secondary');
+})
+
+
 
 function callMovieForm(){
     $('#movieAndSchduleForm').show();
