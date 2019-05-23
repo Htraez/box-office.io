b6980b0080f52bc8f209993a8ee486ace9b14466
Arm <pikaarmz14@hotmail.com>
2019-05-23 20:13:41 +0700

    boy

--- public/scripts/staff.js
+++ public/scripts/staff.js
@@ -8,5 +8,17 @@ function cancelStaff(){
     $(".content-view").show();
 }
 
+function next(){
+    $("#staffForm").hide();
+    $("#addShift").show();
+}
+
+function back(){
+    $("#staffForm").show();
+    $("#addShift").hide();
+}
+
 $(document).on("click","#createStaff",staffForm);
 $(document).on("click","#cancelStaff",cancelStaff);
+$(document).on("click","#next",next);
+$(document).on("click","#back",back)
